97d77f2c23cd85aaa389f54817db6fa7c17c7acf
meo658 <meo658@yahoo.com>
2022-05-27 01:51:31 -0700

    ASS2-Final

--- Question3/script2.js
+++ Question3/script2.js
@@ -1,5 +1,5 @@
 "use strict";
-ư;
+
 const showBtn = document.querySelector("#healthy-btn");
 
 // Add CalculateBMI Button
@@ -8,5 +8,6 @@ BMIBtn.type = "button";
 BMIBtn.classList.add("btn", "btn-warning");
 BMIBtn.id = "calc-BMI-btn";
 BMIBtn.textContent = "Calculate BMI";
+BMIBtn.style = "padding-left: 5px";
 showBtn.after(BMIBtn);
 const calcBMIBtn = document.querySelector("#calc-BMI-btn");
--- assignments/assignment-2/script/data.js
+++ assignments/assignment-2/script/data.js
@@ -1,7 +1,64 @@
 "use strict";
-
 //-DEFINE-
 const activeSideBar = document.querySelector("#sidebar");
+const exportBtn = document.querySelector("#export-btn");
+const importBtn = document.querySelector("#import-btn");
+const fileInput = document.querySelector("#input-file");
+const petArr = getFromStorage("petArr");
+const scriptLink = "http://cdn.jsdelivr.net/g/filesaver.js";
+let importtedPetArr = [];
+
+// FileSaver Library
+// -FUNCTIONS -
+function loadFileSaverScript(src) {
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = src;
+  // Onload to run after undefied function *
+  // script.onreadystatechange = saveDataToFile ;
+  // script.onload = saveDataToFile ;
+  document.querySelector("body").appendChild(script);
+}
+
+function saveDataToFile() {
+  const setFile = new Blob([JSON.stringify(petArr)], {
+    type: "text/plain;charset=utf-8",
+  });
+  saveAs(setFile, "Pets_list.JSON");
+}
+
+function loadDataFromFile() {
+  const getFile = new FileReader();
+  if (fileInput.files[0]) {
+    getFile.readAsText(fileInput.files[0], "UTF-8");
+    getFile.onload = function (e) {
+      try {
+        importtedPetArr = JSON.parse(getFile.result);
+        //Merge import data with current data
+        mergeData(importtedPetArr, petArr);
+        alert("Import Successfully!");
+      } catch (err) {
+        alert(`Error!!!!\n${err}`);
+      }
+    };
+    // Error loading
+    getFile.onerror = function (e) {
+      alert("Error loading JSON file!!!");
+    };
+  } else alert("Please select .JSON file");
+}
+
+function mergeData(fromArr, toArr) {
+  fromArr.forEach((newItem) => {
+    //Update duplicate ID
+    const index = toArr.findIndex((oldItem) => oldItem.id === newItem.id);
+    if (index > -1) {
+      toArr.splice(index, 1, newItem);
+    } else toArr.push(newItem); //Add the rest value
+  });
+  saveToStorage("petArr", toArr); //Save to local
+}
+
 //--MAIN--
 // Animation when click sidebar
 activeSideBar.addEventListener("click", function (e) {
@@ -9,3 +66,9 @@ activeSideBar.addEventListener("click", function (e) {
   if (e.target.contains(activeSideBar))
     activeSideBar.classList.toggle("active");
 });
+// Add Script to html by javascript
+loadFileSaverScript(scriptLink);
+// When click export button
+exportBtn.addEventListener("click", saveDataToFile);
+// When click import button
+importBtn.addEventListener("click", loadDataFromFile);
