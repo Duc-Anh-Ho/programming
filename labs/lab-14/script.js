133a92854a8d48ac519ec783be7b2d2b6e284d3f
meo658 <meo658@yahoo.com>
2022-05-31 03:17:37 -0700

    14

--- labs/lab-14/script.js
+++ labs/lab-14/script.js
@@ -7,26 +7,26 @@ const countriesContainer = document.querySelector(".countries");
 
 const renderCountry = function (data, className = "") {
   console.log(data);
-  console.log(data.flag);
+  console.log(data.flags.png);
   console.log(data.name.common);
   console.log(data.region);
   console.log(data.population);
   console.log(data.languages[Object.keys(data.languages)[0]]);
-  console.log(data.currencies[Object.keys(data.currencies)[0]]);
+  console.log(data.currencies[Object.keys(data.currencies)].name);
   const html = `
   <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
+    <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
       <h3 class="country__name">${data.name.common}</h3>
       <h4 class="country__region">${data.region}</h4>
       <p class="country__row"><span>👫</span>${(
         +data.population / 1000000
-      ).toFixed(1)} people</p>
+      ).toFixed(2)}m people</p>
       <p class="country__row"><span>🗣️</span>${
         data.languages[Object.keys(data.languages)[0]]
       }</p>
       <p class="country__row"><span>💰</span>${
-        data.currencies[Object.keys(data.currencies)[0]]
+        data.currencies[Object.keys(data.currencies)].name
       }</p>
     </div>
   </article>
@@ -43,7 +43,7 @@ const whereAmI = function (lat, lng) {
       return res.json();
     })
     .then((data) => {
-      // console.log(data);
+      console.log(data);
       console.log(`You are in ${data.city}, ${data.country}`);
       return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
     })
@@ -58,6 +58,7 @@ const whereAmI = function (lat, lng) {
     })
     .catch((err) => console.error(`${err.message} 💥💥💥`));
 };
-whereAmI(52.508, 13.381);
-whereAmI(19.037, 72.873);
-whereAmI(-33.933, 18.474);
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+// whereAmI(66.562, 33.821);
