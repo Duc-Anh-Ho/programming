9b0f93b77eab18f1dfaf38187b1f0d5bf5d59151
DUC-ANH <hoducanh123@outlook.com>
2022-05-30 23:55:00 -0700

    14

--- Js/script.js
+++ Js/script.js
@@ -171,140 +171,201 @@
 // secureBooking()();
 // secureBooking()();
 
-// Lap 13.1
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// // Lap 13.1
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} is go ing at ${this.speed}km/h`);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} is go ing at ${this.speed}km/h`);
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is go ing at ${this.speed}km/h`);
-};
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is go ing at ${this.speed}km/h`);
+// };
 
-const bmw = new Car("BMW", 120);
-const mercedes = new Car("Mercedes", 95);
+// const bmw = new Car("BMW", 120);
+// const mercedes = new Car("Mercedes", 95);
 
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
-bmw.brake();
-bmw.accelerate();
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.brake();
+// bmw.brake();
+// bmw.accelerate();
 
-// Lab 13.2
+// // Lab 13.2
 
-class CarCl {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
 
-  accelerate() {
-    this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed}km/h`);
-  }
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`${this.make} is going at ${this.speed}km/h`);
+//   }
 
-  brake() {
-    this.speed -= 5;
-    console.log(`${this.make} is going at ${this.speed}km/h`);
-    return this;
-  }
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`${this.make} is going at ${this.speed}km/h`);
+//     return this;
+//   }
 
-  get speedUS() {
-    return this.speed / 1.6;
-  }
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
 
-  set speedUS(speed) {
-    this.speed = speed * 1.6;
-  }
-}
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
 
-const ford = new CarCl("Ford", 120);
-console.log(ford.speedUS);
-ford.accelerate();
-ford.accelerate();
-ford.brake();
-ford.speedUS = 50;
-console.log(ford);
+// const ford = new CarCl("Ford", 120);
+// console.log(ford.speedUS);
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.speedUS = 50;
+// console.log(ford);
 
-//Lab 13.3
+// //Lab 13.3
 
-const EV = function (make, speed, charge) {
-  // Inheritance properties
-  Car.call(this, make, speed);
-  this.charge = charge;
-};
+// const EV = function (make, speed, charge) {
+//   // Inheritance properties
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
 
-// Inheritance __proto__
-EV.prototype = Object.create(Car.prototype);
+// // Inheritance __proto__
+// EV.prototype = Object.create(Car.prototype);
 
-EV.prototype.chargeEnergy = function (chargeTo) {
-  this.charge = chargeTo;
-};
+// EV.prototype.chargeEnergy = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
 
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge--;
-  console.log(
-    `${this.make} is going at ${this.speed}km/h, with a charge of ${this.charge}%`
-  );
-};
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge--;
+//   console.log(
+//     `${this.make} is going at ${this.speed}km/h, with a charge of ${this.charge}%`
+//   );
+// };
 
-const tesla = new EV("Tesla", 120, 23);
-tesla.chargeEnergy(90);
-console.log(tesla);
+// const tesla = new EV("Tesla", 120, 23);
+// tesla.chargeEnergy(90);
+// console.log(tesla);
 
-tesla.brake();
-tesla.accelerate();
+// tesla.brake();
+// tesla.accelerate();
 
-//Lab 13.4
-class EVCl extends CarCl {
-  #charge; //Private
-  constructor(make, speed, charge) {
-    super(make, speed);
-    this.#charge = charge;
-  }
+// //Lab 13.4
+// class EVCl extends CarCl {
+//   #charge; //Private
+//   constructor(make, speed, charge) {
+//     super(make, speed);
+//     this.#charge = charge;
+//   }
 
-  chargeEnergy(chargeTo) {
-    this.#charge = chargeTo;
-    return this;
-  }
+//   chargeEnergy(chargeTo) {
+//     this.#charge = chargeTo;
+//     return this;
+//   }
 
-  accelerate() {
-    this.speed += 20;
-    this.#charge--;
-    console.log(
-      `${this.make} is going at ${this.speed}km/h, with a charge of ${
-        this.#charge
-      }%`
-    );
-    return this;
-  }
-}
+//   accelerate() {
+//     this.speed += 20;
+//     this.#charge--;
+//     console.log(
+//       `${this.make} is going at ${this.speed}km/h, with a charge of ${
+//         this.#charge
+//       }%`
+//     );
+//     return this;
+//   }
+// }
 
-const rivian = new EVCl("Rivian", 120, 23);
-rivian.accelerate();
-rivian.accelerate();
-rivian.chargeEnergy(80);
-rivian.accelerate();
-rivian.brake();
-rivian
-  .accelerate()
-  .brake()
-  .brake()
-  .brake()
-  .brake()
-  .accelerate()
-  .accelerate()
-  .accelerate()
-  .brake()
-  .accelerate()
-  .accelerate()
-  .accelerate()
-  .accelerate()
-  .brake();
+// const rivian = new EVCl("Rivian", 120, 23);
+// rivian.accelerate();
+// rivian.accelerate();
+// rivian.chargeEnergy(80);
+// rivian.accelerate();
+// rivian.brake();
+// rivian
+//   .accelerate()
+//   .brake()
+//   .brake()
+//   .brake()
+//   .brake()
+//   .accelerate()
+//   .accelerate()
+//   .accelerate()
+//   .brake()
+//   .accelerate()
+//   .accelerate()
+//   .accelerate()
+//   .accelerate()
+//   .brake();
+
+// Lab 14.1
+
+// const btn = document.querySelector(".btn-country");
+// const countriesContainer = document.querySelector(".countries");
+
+// const renderCountry = function (data, className = "") {
+//   const html = `
+//   <article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(
+//         +data.population / 1000000
+//       ).toFixed(1)} people</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//     </div>
+//   </article>
+//   `;
+//   countriesContainer.insertAdjacentHTML("beforeend", html);
+//   countriesContainer.style.opacity = 1;
+// };
+
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText("beforeend", msg);
+//   countriesContainer.style.opacity = 1;
+// };
+
+// const getJSON = function (url, errorMsg = "Something went wrong") {
+//   return fetch(url).then((response) => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+//     return response.json();
+//   });
+// };
+
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then((res) => {
+//       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.country}`);
+
+//       return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+//     })
+//     .then((res) => {
+//       if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+//       return res.json();
+//     })
+//     .then((data) => renderCountry(data[0]))
+//     .catch((err) => console.error(`${err.message} 💥💥💥`));
+// };
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
---
+++ labs/lab-14/script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+// Lab 14
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+const renderCountry = function (data, className = "") {
+  console.log(data);
+  console.log(data.flag);
+  console.log(data.name.common);
+  console.log(data.region);
+  console.log(data.population);
+  console.log(data.languages[Object.keys(data.languages)[0]]);
+  console.log(data.currencies[Object.keys(data.currencies)[0]]);
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${
+        data.languages[Object.keys(data.languages)[0]]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies[Object.keys(data.currencies)[0]]
+      }</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+////
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then((res) => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then((data) => {
+      // console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then((res) => {
+      // console.log(res);
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      return res.json();
+    })
+    .then((data) => {
+      // console.log(data[0].name.common);
+      renderCountry(data[0]);
+    })
+    .catch((err) => console.error(`${err.message} 💥💥💥`));
+};
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
