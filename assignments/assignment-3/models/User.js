3ca5e680016134742b1d700ac8804607529e8743
DUC-ANH <hoducanh123@outlook.com>
2022-06-12 06:59:12 -0700

    Asm-3

--- assignments/assignment-3/models/User.js
+++ assignments/assignment-3/models/User.js
@@ -1,4 +1,9 @@
 "use strict";
+//-DEFINE-
+// Dafault for testing
+const defaultNewsUrl =
+  // "https://newsapi.org/v2/top-headlines?country=us&pageSize=10&page=3&apiKey=9d96b117656d4797a676584025563e2e";
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=9d96b117656d4797a676584025563e2e";
 //-CLASS-
 class User {
   constructor(firstName, lastName, username, password) {
@@ -16,5 +21,19 @@ class User {
       password: this.password,
     });
     saveToStorage("userArr", saveTo);
+  }
+}
+
+class News extends User {
+  constructor(username, url = defaultNewsUrl, API) {
+    super(undefined, undefined, username); //Skip fName and lName)
+    this.url = url;
+    this.API = API;
+  }
+
+  async init() {
+    // return await (await fetch(this.url)).json(); //Oneline fetch and json
+    const response = await fetch(this.url);
+    return await response.json();
   }
 }
--- assignments/assignment-3/scripts/news.js
+++ assignments/assignment-3/scripts/news.js
@@ -1,41 +1,111 @@
 "use strict";
 
 //-DEFINE-
-const newsAPI = "9d96b117656d4797a676584025563e2e";
+//Define Selectors
+const newsContainer = document.querySelector("#news-container");
+//Define Variables
+const currentUser = getFromStorage("currentUser");
+const newsWeb = `https://newsapi.org/v2/`;
+// const newsAPI = "9d96b117656d4797a676584025563e2e"; // Main API
+const newsAPI = "d46449d487d74324a5d01c995cb8f408"; //1 API limited 100 requests/day
 const newsEverything = `everything?`;
 const newsTopHeadline = `top-headlines?`;
-const newsCountryCode = "us";
-const newsCountry = `country=${newsCountryCode}&`;
+const newsCountry = {
+  us: "us",
+  ru: "ru",
+};
+const newsLanguage = {
+  en: "en",
+  ar: "ar",
+  th: "th",
+};
+const newsCategory = {
+  business: "business",
+  entertainment: "entertainment",
+  general: "general",
+  health: "health",
+  science: "science",
+  sports: "sports",
+  technology: "technology",
+};
+const newsSource = {
+  country: `country=${newsCountry.us}&`,
+  language: `language=${newsLanguage.en}&`,
+  category: `category=${newsCategory.business}&`,
+};
+const newsPageSize = "pageSize=" + 10 + "&";
+const newsPage = "page=" + 1 + "&";
 const newsUrl =
-  `https://newsapi.org/v2/` +
+  `${newsWeb}` +
   `${newsTopHeadline}` +
-  `${newsCountry}` +
+  `${newsSource.country}` +
+  `${newsSource.category}` +
   `apiKey=${newsAPI}`;
 
-// // -FUNCTION-
-async function news(url = "") {
-  const response = await fetch(url);
-  console.log(response.url);
-  // return response;
-  const json = await response.json();
-  console.log(json.articles);
-  return json;
-  const articles = await json.articles;
-  // return articles;
-  const thing = await articles[0];
-  return thing;
+const onePageDisplay = 5;
+// -FUNCTION-
+function deleteNewsData() {
+  newsContainer.innerHTML = "";
 }
-
-// // -MAIN-
-// // console.log(postData(newsUrl));
-// // console.log(postData(newsUrl[0]));
-// // console.log(postData(newsUrl[1]));
-// // console.log(postData(newsUrl[2]));
-// const news = postData(newsUrl);
-// console.log(newsUrl);
-news(newsUrl);
-console.log(news(newsUrl));
-// const news2 = fetch(newsUrl).then((res) => res.json());
-// // .then((act) => act.articles[0].author);
-// // .then((data) => data[0].author);
-// console.log(news2);
+function renderHtml(todayNews) {
+  // todayNews.title = todayNews.articles[0].title;
+  // todayNews.description = todayNews.articles[0].description;
+  // todayNews.url = todayNews.articles[0].url;
+  // todayNews.urlToImage = todayNews.articles[0].urlToImage;
+  const newsHTML = `
+    <div class="card flex-row flex-wrap">
+      <div class="card mb-3" style="">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img
+              src="${todayNews.urlToImage}"
+              class="card-img"
+              alt="${todayNews.title}"
+            />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">
+                ${todayNews.title}
+              </h5>
+              <p class="card-text">
+                ${todayNews.description}
+              </p>
+              <a
+                href="${todayNews.url}"
+                class="btn btn-primary"
+              >
+                View
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  return newsHTML;
+}
+function displayNewsData(newsArticlesArr, newsAmount = 5) {
+  // Delete current news
+  deleteNewsData();
+  // Check if don't have enough news display on 1 page
+  if (newsArticlesArr.length < newsAmount)
+    newsAmount = newnewsArticlesArr.length;
+  //renderHTML
+  for (let i = 0; i < newsAmount; i++) {
+    newsContainer.insertAdjacentHTML(
+      "beforeend",
+      renderHtml(newsArticlesArr[i])
+    );
+  }
+}
+// -MAIN-
+const userNews = new News(currentUser[0].username, newsUrl, newsAPI);
+//Anonymous Async Functions
+(async () => {
+  try {
+    const todayNews = await userNews.init();
+    displayNewsData(todayNews.articles, onePageDisplay);
+  } catch (error) {
+    console.error(error);
+  }
+})();
