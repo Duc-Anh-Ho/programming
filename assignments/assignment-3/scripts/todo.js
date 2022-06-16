994c0f96edf1b60c505cb926a64a46af46f64345
meo658 <meo658@yahoo.com>
2022-06-15 19:50:39 -0700

    asm-3

--- assignments/assignment-3/scripts/todo.js
+++ assignments/assignment-3/scripts/todo.js
@@ -1,10 +1,38 @@
 "use strict";
 
+//-DEFINE-
 //Selectors
-//Variables
+const taskInput = document.querySelector("#input-task");
+const todoListContainer = document.querySelector("#todo-list");
+const textTodoList = document.querySelector(".mb-4");
+const addButton = document.querySelector("#btn-add");
+const todoListInputContainer = document.querySelector("#todo-container");
+//Variable
+const currentUser = getFromStorage("currentUser");
+const taskArr = getFromStorage("taskArr");
 // -FUNCTION-
-//Function Declare
-//Asynchronous Function Declare
+function deleteTodoData() {
+  todoListContainer.innerHTML = "";
+}
+function checkLogin(user) {
+  if (user.length === 0) {
+    textTodoList.innerHTML = `Please <a href="./login.html">login</a> to see your Todo List!!!`;
+    todoListInputContainer.innerHTML = "";
+    return false;
+  } else {
+    return true;
+  }
+}
 // -MAIN-
-// Delete current news
-// Login check
+deleteTodoData();
+if (checkLogin(currentUser)) {
+  // taskArr.;
+}
+addButton.addEventListener("click", (e) => {
+  if (taskInput.value.trim()) {
+    console.log("OK", taskInput.value);
+  } else {
+    console.log("NG", taskInput.value);
+  }
+  // const newTask = new Task("taskInput.value")
+});
