2fe4f11450ca119e9fae97074ef6f1f2da861e7b
DUC-ANH <hoducanh123@outlook.com>
2022-07-01 06:33:03 -0700

    day-2

---
+++ 3.JAVA/Mantra.java
@@ -0,0 +1,13 @@
+// 1.7: Mantra
+public class Mantra {
+    public static void main(String[] args) {
+        add();
+        System.out.println();
+        add();
+    }
+
+    public static void add() {
+        System.out.println(
+                "There's one thing every coder must understand:\nThe System.out.println command.");
+    }
+}
