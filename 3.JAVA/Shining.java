30f0f8e475955c1929e531a884e06e7fa8f10139
meo658 <meo658@yahoo.com>
2022-07-02 03:34:29 -0700

    day1

---
+++ 3.JAVA/Shining.java
@@ -0,0 +1,11 @@
+// 1.16: Shining
+public class Shining {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            txt();
+        }
+    }
+    public static void txt(){
+        System.out.println("All work and no play makes Jack a dull boy.");
+    }
+}
