6125d52fbd13af8a15bc890e15c21d38d353f096
DUC-ANH <hoducanh123@outlook.com>
2022-07-03 07:00:20 -0700

    day-2

--- 3.JAVA/Shining.java
+++ 3.JAVA/Shining.java
@@ -1,11 +1,14 @@
 // 1.16: Shining
 public class Shining {
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
-            txt();
-        }
+        int x = 100;
+        txt();
+        System.out.println(txt.getClass().getSimpleName());
+        System.out.println(((Object) x).getClass().getSimpleName());
+
     }
-    static public void txt(){
+
+    static public void txt() {
         System.out.println("All work and no play makes Jack a dull boy.");
     }
 }
