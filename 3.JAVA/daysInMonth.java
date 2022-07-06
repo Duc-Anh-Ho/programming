28e1016a50360baed1052af9654d2bc60319003e
DUC-ANH <hoducanh123@outlook.com>
2022-07-06 04:27:06 -0700

    day-3

---
+++ 3.JAVA/daysInMonth.java
@@ -0,0 +1,17 @@
+
+// 4.4: daysInMonth
+import java.util.Scanner;
+
+public class daysInMonth {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String num = input.nextLine();
+            System.out.println(num);
+            if (num.equals("0"))0 {
+                System.out.println("OK");
+                break;
+            }
+        }
+    }
+}
