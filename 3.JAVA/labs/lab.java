19ef85333026f098978a32691106a48b1835be69
DUC-ANH <hoducanh123@outlook.com>
2022-07-20 07:03:16 -0700

    day-6

--- 3.JAVA/labs/lab.java
+++ 3.JAVA/labs/lab.java
@@ -1,15 +1,21 @@
-
-// 7.8: median
-import java.util.Arrays;
-
+// 7.14: contains 
 public class lab {
     public static void main(String[] args) {
-        int[] arr = { 74, 85, 102, 99, 101, 56, 84 };
-        System.out.println(median(2, arr));
+        int[] arr1 = { 1, 6, 2, 1, 4, 1, 2, 1, 8 };
+        int[] arr2 = { 1, 2, 2 };
+        System.out.println(contains(arr1, arr2));
     }
 
-    public static int median(int numOrder, int[] arr) {
-        Arrays.sort(arr);   
-        return arr[arr.length - numOrder - 1] ;
+    public static boolean contains(int[] arr1, int[] arr2) {
+        int temp = 0;
+        int[] longerArr = arr1;
+        int[] shorterArr = arr2;
+        if (arr2.length > arr1.length) {
+            longerArr = arr2;
+            shorterArr = arr1;
+        }
+        for (int i = 0; i < shorterArr.length; i++)
+
+            return false;
     }
 }
