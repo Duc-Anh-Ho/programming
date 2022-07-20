2d25289f5ef19cb53c68dbe6166f2ad7bd7763da
meo658 <meo658@yahoo.com>
2022-07-20 04:41:27 -0700

    day-6

--- 3.JAVA/labs/lab.java
+++ 3.JAVA/labs/lab.java
@@ -1,17 +1,15 @@
-// 7.7: kthLargest
+
+// 7.8: median
+import java.util.Arrays;
+
 public class lab {
     public static void main(String[] args) {
         int[] arr = { 74, 85, 102, 99, 101, 56, 84 };
-        System.out.println(kthLargest(0, arr));
+        System.out.println(median(2, arr));
     }
 
-    public static int kthLargest(int numOrder, int[] arr) {
-        int temp = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (temp < arr[i]) {
-                arr[i] = temp;
-            }
-        }
-        return arr[numOrder];
+    public static int median(int numOrder, int[] arr) {
+        Arrays.sort(arr);   
+        return arr[arr.length - numOrder - 1] ;
     }
 }
