accc981b3f6c7debfd92eeaf00a0d23a21c475f1
DUC-ANH <hoducanh123@outlook.com>
2022-07-21 07:18:24 -0700

    day-6

--- 3.JAVA/labs/lab.java
+++ 3.JAVA/labs/lab.java
@@ -1,39 +1,23 @@
-// 7.14: contains 
+
+// 7.15: collapse
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class lab {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 1, 2, 3};
-        int[] arr2 = {1, 2, 3};
-        // int[] arr1 = {1, 2, 1};
-        // int[] arr2 = {1, 2, 2, 2, 5, 7, 9, 9, 9};
-        System.out.println(contains(arr1, arr2));
+        int[] arr = { 7, 2, 8, 9, 4, 13, 7, 1, 9, 10 };
+        // int[] arr = { 1, 2, 3, 4, 5 };
+        System.out.println(Arrays.toString(collapse(arr)));
     }
 
-    public static boolean contains(int[] arr1, int[] arr2) {
-        int temp = 0;
-        int[] longerArr = arr1;
-        int[] shorterArr = arr2;
-        if (arr2.length > arr1.length) {
-            longerArr = arr2;
-            shorterArr = arr1;
+    public static int[] collapse(int[] arr) {
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length / 2; i++) {
+            arrList.add(arr[i * 2] + arr[i * 2 + 1]);
         }
-        System.out.println("shorterArr " +shorterArr.length);
-        System.out.println("longerArr " +longerArr.length);
-        for (int i = 0; i < longerArr.length; i++) {
-            System.out.println("i " + i + " temp " + temp);
-            if (longerArr[i] == shorterArr[temp]) {
-                temp++;
-            } 
-            else {
-                temp = 0;
-                // Check again when reset temp 
-                if (longerArr[i] == shorterArr[temp]) {
-                  temp++;
-                }
-            }
-            if (temp == shorterArr.length) {
-                return true;
-            }
+        if (arr.length % 2 == 1) {
+            arrList.add(arr.length);
         }
-    return false;
+        return arrList.stream().mapToInt(i -> i).toArray();
     }
 }
