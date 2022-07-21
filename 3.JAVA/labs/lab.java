b39d9ca02669106b351dd051fef8c5534febc0e2
meo658 <meo658@yahoo.com>
2022-07-20 20:31:50 -0700

    day-6

--- 3.JAVA/labs/lab.java
+++ 3.JAVA/labs/lab.java
@@ -1,8 +1,10 @@
 // 7.14: contains 
 public class lab {
     public static void main(String[] args) {
-        int[] arr1 = { 1, 6, 2, 1, 4, 1, 2, 1, 8 };
-        int[] arr2 = { 1, 2, 2 };
+        int[] arr1 = {1, 2, 1, 2, 3};
+        int[] arr2 = {1, 2, 3};
+        // int[] arr1 = {1, 2, 1};
+        // int[] arr2 = {1, 2, 2, 2, 5, 7, 9, 9, 9};
         System.out.println(contains(arr1, arr2));
     }
 
@@ -14,8 +16,24 @@ public class lab {
             longerArr = arr2;
             shorterArr = arr1;
         }
-        for (int i = 0; i < shorterArr.length; i++)
-
-            return false;
+        System.out.println("shorterArr " +shorterArr.length);
+        System.out.println("longerArr " +longerArr.length);
+        for (int i = 0; i < longerArr.length; i++) {
+            System.out.println("i " + i + " temp " + temp);
+            if (longerArr[i] == shorterArr[temp]) {
+                temp++;
+            } 
+            else {
+                temp = 0;
+                // Check again when reset temp 
+                if (longerArr[i] == shorterArr[temp]) {
+                  temp++;
+                }
+            }
+            if (temp == shorterArr.length) {
+                return true;
+            }
+        }
+    return false;
     }
 }
