b7ae3b84f68a8bd930e1e243fbb904eca9f5f761
DUC-ANH <hoducanh123@outlook.com>
2022-07-23 07:18:59 -0700

    day-6

--- 3.JAVA/labs/lab.java
+++ 3.JAVA/labs/lab.java
@@ -1,23 +1,29 @@
 
-// 7.15: collapse
-import java.util.ArrayList;
+// 7.18: matrixAdd 
 import java.util.Arrays;
 
 public class lab {
     public static void main(String[] args) {
-        int[] arr = { 7, 2, 8, 9, 4, 13, 7, 1, 9, 10 };
-        // int[] arr = { 1, 2, 3, 4, 5 };
-        System.out.println(Arrays.toString(collapse(arr)));
+        // int arr1[][] = { { 1, 2, 3, 4, 5 }, { 1, 1, 2, 3, 4 } };
+        // int arr2[][] = { { 3, 2, 6, 4, 5 }, { 1, 1, 2, 3, 4 } };
+        int arr1[][] = {};
+        int arr2[][] = {};
+        System.out.println(arr1.length);
+        System.out.println(Arrays.deepToString(matrixAdd(arr1, arr2)));
+
     }
 
-    public static int[] collapse(int[] arr) {
-        ArrayList<Integer> arrList = new ArrayList<Integer>();
-        for (int i = 0; i < arr.length / 2; i++) {
-            arrList.add(arr[i * 2] + arr[i * 2 + 1]);
+    public static int[][] matrixAdd(int arr1[][], int arr2[][]) {
+        int[][] arr = {};
+        if (arr1.length == 0) {
+            return arr;
         }
-        if (arr.length % 2 == 1) {
-            arrList.add(arr.length);
+        arr = new int[2][arr1[0].length];
+        for (int i = 0; i < arr1[0].length; i++) {
+            arr[0][i] = arr1[0][i] + arr2[0][i];
+            arr[1][i] = arr1[1][i] + arr2[1][i];
         }
-        return arrList.stream().mapToInt(i -> i).toArray();
+        return arr;
     }
+
 }
